bf1f6f2f4217b32eb90eef5ca69ed53988bc6453
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 17:26:54 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  const int regular_hours = 40;
+  const int work_hours = 50;
+  const double overtime_rate = 27.78;
+  const double regular_rate = 18.25;
+  double total_wage;
+
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Total wage is: " << total_wage << endl;
+}
